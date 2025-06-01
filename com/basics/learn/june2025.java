6e1e2134073740e56db2b67e37e4f0ca312ebabb
Jpurna <jpurna86@gmail.com>
2025-06-01 13:38:47 -0400

    Committed second commit june-2025 branch.

--- com/basics/learn/june2025.java
+++ com/basics/learn/june2025.java
@@ -3,6 +3,6 @@ package com.basics.learn;
 public class june2025 {
 
     public static void main(String[] args) {
-        System.out.println("sample file");
+        System.out.println("sample file1");
     }
 }
